78d30a355344e222e733fbcf101d572fa0e82031
Basile <basile.brunet@gmail.com>
2020-05-14 17:33:02 +0200

    modif vsftpd conf

--- ftps/start.sh
+++ ftps/start.sh
@@ -3,6 +3,6 @@
 echo "switching to default runlevel..."
 openrc default &> /dev/null
 echo "starting ftps..."
-rc-service ftps start &> /dev/null
+rc-service vsftpd start &> /dev/null
 echo "all set !"
 tail -f /dev/null
--- influxdb/start.sh
+++ influxdb/start.sh
@@ -2,9 +2,8 @@
 
 echo "switching to default runlevel..."
 openrc default &> /dev/null
-echo "starting grafana..."
-
-rc-service influxdb start -D &> /dev/null
+echo "starting influxdb..."
+rc-service influxdb start &> /dev/null
 
 echo "all set !"
 tail -f /dev/null
