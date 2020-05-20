79dccb5fa259188da8951da08d1c6d5e63576ad5
Basile <basile.brunet@gmail.com>
2020-05-20 15:39:27 +0200

    ok monitoring des containers

--- influxdb/start.sh
+++ influxdb/start.sh
@@ -7,5 +7,11 @@ rc-service influxdb start &> /dev/null
 #"restart" because didnt create the database with start alone...
 rc-service telegraf restart &> /dev/null
 
+#correspond au socket vers docker-daemon (pour que telegraf ait accès à l'activité des containers)
+chown root:root /var/run/docker.sock
+chmod 666 /var/run/docker.sock
+###comprendre pourquoi cela ne suffit pas de faire un chown root:root, en gardant le chmod initial (660)
+
+
 echo "all set !"
 tail -f /dev/null
