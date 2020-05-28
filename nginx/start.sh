38a7320ce576c89d84cc97ef5004306eb5a50f02
Basile <basile.brunet@gmail.com>
2020-05-28 16:19:36 +0200

    access nginx en ssh

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-echo "Starting Nginx !"
+openrc default
+rc-service sshd start
+
+#on doit changer le mot de passe du root pour pouvoir se connecter à celui ci
+echo "root:basile42" | chpasswd
+
 nginx
-echo "Starting Php"
 /usr/sbin/php-fpm7
 
 tail -f /dev/null
--- script.sh
+++ script.sh
@@ -1,33 +1,24 @@
-#TODO:
-# faire marcher wordpress au démarrage (phase d'instalation qui plante)
-## probléme car php serveur un peu lent
-## problemes de login logout... (rewrite ?)
-## solution JN: copier la base de donnée sql une fois remplie, pour ne pas avoir à refaire l'instalation
-## problème: erreur sur la BDD de mon coté
-
-# script shell
-# mettre les bases de données dans des pv, s'assurer qu'on les retrouve en cas de crash/redemarage
-# tester telechargement de fichier sur serveur ftp
-# acces a nginx via ssh
-# dashboard grafana: 1 par service
-
-
 #!/bin/bash
+
+#install openssh-client if not installed
+
 minikube start --driver=virtualbox
 eval $(minikube docker-env)
 #dashboard and ingress
 minikube addons enable ingress
 minikube addons enable dashboard
-#cmd to access dashboard
-minikube dashboard
 
 #1) put minik_ip into a global variable
 export MINIK_IP=$(minikube ip)
 #2) set MINIK_IP where it needs to: 
-#export MINIK_IP into an environment variable of grafana containers
-#env variable will be used in grafana.ini
+#export MINIK_IP into an environment variable of grafana containers. Env variable will be used in grafana.ini
 sed -i "s/\$MINIK_IP/$MINIK_IP/" grafana_deployment.yaml
 #set MINIK_IP in vsftpd.conf
 sed -i "s/\$MINIK_IP/$MINIK_IP/" ftps/vsftpd.conf
 #set MINIK_IP in wp-config.php 
 sed -i "s/\$MINIK_IP/$MINIK_IP/" wordpress/wp-config.php
+
+#build images
+cd mysql
+docker build -t mysql .
+cd ..
