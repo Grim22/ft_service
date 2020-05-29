f313fdd69a950020cd56518708a8b631953da7c3
Basile <basile.brunet@gmail.com>
2020-05-29 11:30:14 +0200

    MINIK_IP set correctly in grafana

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 
 #install openssh-client if not installed
 
-minikube start --driver=virtualbox
+echo ">>>>>>>>> starting minikube"
+#minikube start --driver=virtualbox
 eval $(minikube docker-env)
 #dashboard and ingress
 minikube addons enable ingress
@@ -13,14 +14,28 @@ export MINIK_IP=$(minikube ip)
 
 ## ATTENTION: vérifier que dans les endroits où l'on veut changer la minik_ip, l'ip n'est pas rentrée en dur 
 #2) set MINIK_IP where it needs to: 
-#export MINIK_IP into an environment variable of grafana containers. Env variable will be used in grafana.ini
-sed -i "s/\$MINIK_IP/$MINIK_IP/" grafana.yaml
+#in grafana.ini
+sed -i "s/\$MINIK_IP/$MINIK_IP/" grafana/grafana.ini
 #set MINIK_IP in vsftpd.conf
 sed -i "s/\$MINIK_IP/$MINIK_IP/" ftps/vsftpd.conf
 #set MINIK_IP in wp-config.php 
 sed -i "s/\$MINIK_IP/$MINIK_IP/" wordpress/wp-config.php
 
-#build images
-cd mysql
-docker build -t mysql .
+echo ">>>>>>>>> buiding images"
+services=(influxdb mysql wordpress grafana nginx ftps phpmyadmin)
+for service in ${services[*]}
+do
+cd $service
+docker build -t $service .
 cd ..
+done
+
+echo ">>>>>>>>> starting deployment"
+for service in ${services[*]}
+do
+kubectl apply -f $service.deployment
+done
+kubectl apply -f ingress.yaml
+
+
+#build images
