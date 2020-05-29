39c437ca0e483d7a140180714b64dd1ef96ea89e
Basile <basile.brunet@gmail.com>
2020-05-29 18:41:45 +0200

    dashboard not provisionning

--- grafana/start.sh
+++ grafana/start.sh
@@ -1,10 +1,4 @@
 #!bin/sh
 
-openrc default &> /dev/null
-
-#ne marche pas si on lance grafana 'comme d'habitude. Peut être parce qu'il le lance en user=grafana plutot que root ?
-#methode: lancer directement le binaire
-#tourne au premier plan: pas besoin de tail -f
-
-/usr/sbin/grafana-server --config=/etc/grafana.ini --homepath=/usr/share/grafana
+#cd /grafana-7.0.1/bin/ && ./grafana-server
 tail -f /dev/null
