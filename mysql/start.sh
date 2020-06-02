d4c7d0b51d1337cc6526d38960061457926a51ee
Basile <basile.brunet@gmail.com>
2020-06-02 15:57:45 +0200

    fixed php bug and downloaded php script to fill wordpress database - with one admin and two users

--- mysql/start.sh
+++ mysql/start.sh
@@ -6,7 +6,9 @@ echo "seting up mariadb..."
 rc-service mariadb setup &> /dev/null
 echo "starting mariadb..."
 rc-service mariadb start &> /dev/null
-echo "creating database..."
+#create database wordpress and mysql user with access to created database
 mysql -u root < tmp/database.sql
+#fills database (sql file obtained exported from phpmyadmin, after wordpress was configured with one admin and two users. Line 13 added to set the database to wordpress)
+mysql -u root < tmp/wordpress.sql
 echo "all set !"
 tail -f /dev/null
--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-php -S 0.0.0.0:5500 -t /var/www/localhost/htdocs/
+php -S 0.0.0.0:5500 -t /var/tmp/
