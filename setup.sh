bb9b4d3e834d9855b0d4c379f2bb8a7be1be88c9
Basile <basile.brunet@gmail.com>
2020-06-10 17:05:06 +0200

    rename according to rule

--- setup.sh
+++ setup.sh
@@ -23,21 +23,13 @@ minikube addons enable dashboard
 #above line does not work sometimes with driver=docker, as minikube ip would return 127.0.0.1. Corrected by using the line below to get the real minikube ip
 export MINIK_IP="$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)"
 
+cd srcs
 
 ## ATTENTION: vérifier que dans les endroits où l'on veut changer la minik_ip, l'ip n'est pas rentrée en dur 
 #2) set MINIK_IP where it needs to: 
-#in grafana.ini
-#sed -i "s/\$MINIK_IP/$MINIK_IP/" grafana/defaults.ini
-#set MINIK_IP in vsftpd.conf
-#sed -i "s/\$MINIK_IP/$MINIK_IP/" ftps/vsftpd.conf
-#set MINIK_IP in wp-config.php 
-#sed -i "s/\$MINIK_IP/$MINIK_IP/" wordpress/wp-config.php
-#set MINIK_IP in nginx index and in wordpress.sql
 sed -i "s/\$MINIK_IP/$MINIK_IP/" nginx/index.html
 sed -i "s/\$MINIK_IP/$MINIK_IP/" mysql/wordpress.sql
 
-
-##VERIFIER QUE CA MARCHE BIEN
 eval $(minikube docker-env)
 
 ## ATTENTION a bien lancer grafana APRES influxdb car grafana a besoin de connaitre l'IP du service influxdb (est exporté automatiquement dans les variables d'env)
